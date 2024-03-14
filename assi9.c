0c63be38b32bce2bd9a108ea30d8dff793180a6e
Omkar821 <omkarmore8284@gmail.com>
2024-03-14 18:18:22 +0530

    c_assignments

---
+++ assi9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+float d,T;
+
+printf("enter the temperature in fahrenhett=");
+scanf("%f",&d);
+
+T=5*(d-32)/9;
+printf("tempreture in celsius is=%2f\n",T);
+	return 0;
+
+
+
+
+}
